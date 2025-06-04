593bfe6da5d927c65796027e2c04cfd8e783322f
Kaizoku2003 <jdcenturion1@gmail.com>
2025-06-04 00:33:27 -0300

    Update boton.py
    Lo mismo, está en comentarios...

--- boton.py
+++ boton.py
@@ -4,35 +4,35 @@ import const2
 
 
 class Boton:
-    def __init__(self,texto, fuente, color_normal, color_hover, posicion):
+    def __init__(self,texto, fuente, color_normal, color_hover, y_pos):
        self.texto = texto
        self.fuente = fuente
        self.color_normal = color_normal
        self.color_efecto = color_hover
-       self.posicion = posicion
+       self.y_pos = y_pos
        self.color_actual = color_normal
-       self.rect = None
+       self.posicion = self._calcular_posicion_centrada()
 
-    def dibujar (self, ventana, posicion_mouse):
+    def _calcular_posicion_centrada(self):
+        texto_temp = self.fuente.render(self.texto, True, self.color_normal)
+        x_centrado = (const2.width - texto_temp.get_width()) // 2
+        return (x_centrado, self.y_pos)
 
-        if self.clic(posicion_mouse):
-            self.color_actual = self.color_efecto
-        else:
-            self.color_actual = self.color_normal
+    def dibujar(self, ventana, posicion_mouse):
+        # Color al pasar el mouse
+        self.color_actual = self.color_efecto if self.clic(posicion_mouse) else self.color_normal
 
-        """Dibuja el botón y cambia de color si el mouse está encima."""
-        # Creacion de la sombra
-        sombra_render = self.fuente.render(self.texto, True, const2.negro)
-        sombra_rect = sombra_render.get_rect(center=(self.posicion[0] - 5, self.posicion[1] + 5))
-        ventana.blit(sombra_render, sombra_rect)
+        # Sombra (5px abajo/derecha)
+        sombra = self.fuente.render(self.texto, True, const2.negro)
+        ventana.blit(sombra, (self.posicion[0] - const2.DESPLAZAMIENTO_SOMBRA,
+                              self.posicion[1] + const2.DESPLAZAMIENTO_SOMBRA))
 
-        # Se renderiza el texto con el color actual
-        texto_render = self.fuente.render(self.texto, True, self.color_actual)
-        texto_rect = texto_render.get_rect(center=self.posicion)
-        ventana.blit(texto_render, texto_rect)
+        # Texto principal
+        texto = self.fuente.render(self.texto, True, self.color_actual)
+        ventana.blit(texto, self.posicion)
 
     def clic(self, posicion_mouse):
-
-        texto_render = self.fuente.render(self.texto, True, self.color_actual)
-        texto_rect = texto_render.get_rect(center=self.posicion)
+        texto_rect = self.fuente.render(self.texto, True, self.color_actual).get_rect(
+            topleft=self.posicion
+        )
         return texto_rect.collidepoint(posicion_mouse)
