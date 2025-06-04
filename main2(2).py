d0c3628d781609838f30f3d2f56a947054cac8ed
SuarezBenja <benjaasuarezz11@gmail.com>
2025-06-04 20:08:07 -0300

    Update main2(2).py

--- main2(2).py
+++ main2(2).py
@@ -1,8 +1,11 @@
 from juego2 import Juego
 from login import pantalla_login
 
-if pantalla_login():  # Solo continúa si se inicia sesión correctamente
+login_exitoso, username = pantalla_login()
+
+if login_exitoso:  # Solo continúa si se inicia sesión correctamente
     g = Juego()
+    g.username = username  # Asignar el nombre de usuario
     while g.run:
         g.play = True
         g.bucle_juego()
